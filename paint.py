f2fac786b679fbbffc29c6578fa4908c7b2dfaa4
Eduardo <juantokx@gmail.com>
2025-10-30 11:50:02 -0600

    - Implementada función para dibujar un rectángulo - Se usa el punto inicial y final - Se usa un for para dibujar el rectángulo

--- paint.py
+++ paint.py
@@ -38,7 +38,24 @@ def circle(start, end):
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+
+    width = end.x - start.x
+    height = end.y - start.y
+
+
+    for _ in range(2):
+        forward(width)
+        left(90)
+        forward(height)
+        left(90)
+
+
+    end_fill()
 
 def triangle(start, end):
     "Draw triangle from start to end."
